8a53480ff70604ad8f2164004aec1af126c00a8d
tmldoc <huthaifamahmad@gmail.com>
2021-03-30 11:50:07 +0300

    working loop

--- lib/videoAnimationTile.dart
+++ lib/videoAnimationTile.dart
@@ -2,20 +2,20 @@ import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/screen_util.dart';
 import 'package:inview_notifier_list/inview_notifier_list.dart';
+import 'package:provider/provider.dart';
+import 'package:reels_ui_animation/scrollProvider.dart';
 import 'package:reels_ui_animation/userProfileImageLikeCommentShare.dart';
 import 'package:reels_ui_animation/videoPlayerWithControls.dart';
 import 'allComments.dart';
 
 class VideoAnimationTile extends StatefulWidget {
   final String url;
-  VideoAnimationTile(
-      {@required this.url});
+  VideoAnimationTile({@required this.url});
   @override
   _VideoAnimationTileState createState() => _VideoAnimationTileState();
 }
 
-class _VideoAnimationTileState extends State<VideoAnimationTile>
-    with SingleTickerProviderStateMixin {
+class _VideoAnimationTileState extends State<VideoAnimationTile> with SingleTickerProviderStateMixin {
   BetterPlayerController _betterPlayerController;
   AnimationController _controller;
   double iconsHeightAndWidth;
@@ -24,16 +24,20 @@ class _VideoAnimationTileState extends State<VideoAnimationTile>
   @override
   void initState() {
     /*Animation Controller To Control Video ViewPort Size*/
-    _controller =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
 
-    BetterPlayerConfiguration betterPlayerConfiguration =
-        BetterPlayerConfiguration(
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+
+    BetterPlayerConfiguration betterPlayerConfiguration = BetterPlayerConfiguration(
       fit: BoxFit.fill,
       aspectRatio: (9 / 16),
       //Turned visibility off of icons by setting value to false.
-      controlsConfiguration:
-          BetterPlayerControlsConfiguration(showControls: false),
+      controlsConfiguration: BetterPlayerControlsConfiguration(showControls: false),
     );
 
     //  Setting Up Data Source
@@ -49,15 +53,13 @@ class _VideoAnimationTileState extends State<VideoAnimationTile>
 
     _betterPlayerController = BetterPlayerController(betterPlayerConfiguration);
     _betterPlayerController.setupDataSource(dataSource);
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     //Calculating working Area.
-    double fullVideoViewPortHeight = MediaQuery.of(context).size.height -
-        kBottomNavigationBarHeight -
-        ScreenUtil().statusBarHeight;
+    double fullVideoViewPortHeight =
+        MediaQuery.of(context).size.height - kBottomNavigationBarHeight - ScreenUtil().statusBarHeight;
 
     return Container(
       width: MediaQuery.of(context).size.width,
@@ -68,11 +70,9 @@ class _VideoAnimationTileState extends State<VideoAnimationTile>
           AnimatedBuilder(
             animation: _controller,
             builder: (_, Widget child) {
-
               //Calculating icons Height And Width For Responsiveness.
               iconsHeightAndWidth = (fullVideoViewPortHeight * 1) * 0.05 -
-                  (((fullVideoViewPortHeight * 1) * 0.05) / 2) *
-                      _controller.value;
+                  (((fullVideoViewPortHeight * 1) * 0.05) / 2) * _controller.value;
 
               return Column(
                 children: [
@@ -81,17 +81,16 @@ class _VideoAnimationTileState extends State<VideoAnimationTile>
                     fit: BoxFit.fill,
                     child: SizedBox(
                       //Both Height and width are Responsible for viewport height and width change.
-                      height: fullVideoViewPortHeight -
-                          fullVideoViewPortHeight * 0.6 * _controller.value,
-                      width: MediaQuery.of(context).size.width-MediaQuery.of(context).size.width*0.6*_controller.value,
+                      height: fullVideoViewPortHeight - fullVideoViewPortHeight * 0.6 * _controller.value,
+                      width: MediaQuery.of(context).size.width -
+                          MediaQuery.of(context).size.width * 0.6 * _controller.value,
                       child: AspectRatio(
-                        aspectRatio: 9/16,
+                        aspectRatio: 9 / 16,
                         child: InViewNotifierWidget(
                           id: widget.url,
-                          builder:
-                              (BuildContext context, bool isInView, Widget child) {
-                            _betterPlayerController.setLooping(true);
+                          builder: (BuildContext context, bool isInView, Widget child) {
                             if (isInView) {
+                              _betterPlayerController.setLooping(true);
                               _betterPlayerController.play();
                             } else {
                               _betterPlayerController.pause();
@@ -99,12 +98,9 @@ class _VideoAnimationTileState extends State<VideoAnimationTile>
                             return Stack(
                               children: [
                                 /*Volume 1 equal to 100% */
-                                VideoPlayerWithControls(
-                                    betterPlayerController:
-                                        _betterPlayerController),
+                                VideoPlayerWithControls(betterPlayerController: _betterPlayerController),
                                 UserProfileImageLikeCommentShare(
-                                    iconsHeightAndWidth: iconsHeightAndWidth,
-                                    controller: _controller)
+                                    iconsHeightAndWidth: iconsHeightAndWidth, controller: _controller)
                               ],
                             );
                           },
@@ -116,9 +112,7 @@ class _VideoAnimationTileState extends State<VideoAnimationTile>
                   //All Comments
                   Align(
                     alignment: Alignment.bottomCenter,
-                    child: AllComments(
-                        fullVideoViewPortHeight: fullVideoViewPortHeight,
-                        controller: _controller),
+                    child: AllComments(fullVideoViewPortHeight: fullVideoViewPortHeight, controller: _controller),
                   ),
                 ],
               );
